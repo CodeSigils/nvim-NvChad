7a82061519fb068ceefaa848db1fd42a32b78be2
CodeSigils <web_tools88@yahoo.com>
2025-02-25 11:32:15 +0200

    update conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,17 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    bash = {"shfmt"},
-    python = {"black"}
-    -- css = { "prettierd" },
-    -- html = { "prettierd" },
+    bash = { "shfmt" },
+    python = { "black" },
+    css = { "prettierd" },
+    html = { "prettierd" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
