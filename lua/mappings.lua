0c2608a84928f82bce9e22dd7a60b1a627f2a178
CodeSigils <web_tools88@yahoo.com>
2025-02-25 11:35:57 +0200

    leader q to quit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,5 @@ map("i", "ii", "<ESC>")
 
 -- Ctrl+s for classic save
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Space + q to close nvchad
+map("n", "<leader>q", "<cmd>qa<cr>", { desc = "Close Neovim" })
