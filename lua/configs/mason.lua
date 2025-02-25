1cfda128d9d0345257f3c6cdbdd520bc56db0e0c
CodeSigils <web_tools88@yahoo.com>
2025-02-25 12:29:01 +0200

    Add separate mason file into configs

---
+++ lua/configs/mason.lua
@@ -0,0 +1,17 @@
+local options = {
+  ensure_installed = {
+    "lua_language-server",
+    "luacheck",
+    "stylua",
+    "bash-language-server",
+    "shfmt",
+    "black",
+    "html-lsp",
+    "css-lsp",
+    "prettierd",
+    "marksman",
+    "yaml-language-server",
+  },
+}
+
+return options
